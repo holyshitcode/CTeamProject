3734a46bfeeee7bd837a77b123bcab2310b8858a
Uzih05 <luv.wlgjs@gmail.com>
2024-11-22 09:18:03 +0900

    new

---
+++ cmake-build-debug/CMakeFiles/CteamProject.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CteamProject.dir/board.c.o"
+  "CMakeFiles/CteamProject.dir/board.c.o.d"
+  "CMakeFiles/CteamProject.dir/list.c.o"
+  "CMakeFiles/CteamProject.dir/list.c.o.d"
+  "CMakeFiles/CteamProject.dir/main.c.o"
+  "CMakeFiles/CteamProject.dir/main.c.o.d"
+  "CMakeFiles/CteamProject.dir/message.c.o"
+  "CMakeFiles/CteamProject.dir/message.c.o.d"
+  "CteamProject"
+  "CteamProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CteamProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
