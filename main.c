52a6389ff0758cddd04eb23d0775e42be9473fbc
junyeong <557987@naver.com>
2024-11-25 14:57:06 +0900

    register gui

--- main.c
+++ main.c
@@ -9,14 +9,18 @@ int main() {
     // fclose(fp);
     // fclose(fp2);
     initscr();
-    FILE *fp = fopen("userInfo.txt", "r");
-    loadMember(fp);
-    fclose(fp);
-
-    UserInfo *user = findUser("a");
-    printf("\n%s\n", user->nickname);
-    int a = userLogin("b","c");
-    printf("%d\n", a);
+    // FILE *fp = fopen("userInfo.txt", "r");
+    // loadMember(fp);
+    // fclose(fp);
+    //
+    // UserInfo *user = findUser("a");
+    // printf("\n%s\n", user->nickname);
+    // int a = userLogin("b","c");
+    // printf("%d\n", a);
+    // int b = userRegister(fp2,"bb","cc","dd");
+    // printf("b=%d\n", b);
+    // printf("login = %d\n",userLogin("cc","dd"));
+    // fclose(fp2);
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
